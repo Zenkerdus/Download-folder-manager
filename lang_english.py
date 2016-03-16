a7b3208226f0d2c0d803020bba71af9cf93f4a93
Zenkerdus <zenkendun@gmail.com>
2016-03-16 21:53:23 +0100

    Language file. Not implemented yet

---
+++ lang_english.py
@@ -0,0 +1,34 @@
+"""English language file"""
+
+MSG_ERROR_CORRUPTINI = "--Error: Couldn't read path. Corrupt ini file?"
+MSG_DOWNLOADFOLDER = "Downloadfolder: "
+MSG_ERROR_PATHNOTFOUND = "--Error: Path not found! Did you type it correctly in the config file?"
+MSG_MENU = ["What do you want to do?",
+            "1.Show files",
+            "2.Move files",
+            "3.Create folders",
+            "4.Remove junk files",
+            "5.DEBUG-Move files back",
+            "6.Count filetypes in downloadfolder",
+            "Q/0 - Quit"]
+MSG_CHOICE = "Choice> "
+MSG_INVALIDCHOICE = "-- Invalid choice!"
+MSG_FILELIST = "  ---Filelist---"
+MSG_EXISTS = " exists"
+MSG_CREATED = " created"
+MSG_ERROR_FOLDERS = "--Error: Have you created folders first? Choose 3 in menu"
+MSG_ERROR_MESSAGE = "--Message: "
+MSG_ERROR_FILEEXISTS = "--Error: File already exists!"
+MSG_CHOICE_REMOVE = "Remove? (Y/N)"
+MSG_YES = ["Y", "YES"]
+MSG_REMOVED = "Removed "
+MSG_NO = ["N", "NO"]
+MSG_ERROR_FILEUSED = "--Error: File is being used!"
+MSG_MOVED = "Moved "
+MSG_TO = " to "
+MSG_NOVALIDFILES = "No valid files to sort"
+MSG_FILES = " files"
+MSG_ERROR_DOESNTEXIST = "--Error: File/folder doesn't exist. Skipping"
+MSG_REMOVEFROMSORTED = "Remove from sorted folder? (Y/N)"
+MSG_QUITTING = "Quitting..."
+
